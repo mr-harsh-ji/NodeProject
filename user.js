3a44502d0f5885fce609fc08f42e1f6488ff6bb3
Harsh Kumar <harshkumarsoni1@gmail.com>
2025-10-30 13:02:52 +0530

    error solved

--- user.js
+++ user.js
@@ -53,40 +53,40 @@ app.delete("/users/:id",(request, response)=>{
 
 //password hashing - register
 
-app.post("/api/users", async (request, response)=>{
-    const name = request.body.name;
-    const email = request.body.email;
-    const password = request.body.password;
-    const passwordHash = await bcrypt.hash(password, 10)// 2 ka power 10 standard
+// app.post("/api/users", async (request, response)=>{
+//     const name = request.body.name;
+//     const email = request.body.email;
+//     const password = request.body.password;
+//     const passwordHash = await bcrypt.hash(password, 10)// 2 ka power 10 standard
     
-    db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[name, email, passwordHash],(error,result)=>{
-    console.log(error);
-    if(error) return response.status(500).json({message : "Server internal error" + error});
-    response.status(201).json({id: result.insertId, name : name, email: email});
-    });
-});
+//     db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[name, email, passwordHash],(error,result)=>{
+//     console.log(error);
+//     if(error) return response.status(500).json({message : "Server internal error" + error});
+//     response.status(201).json({id: result.insertId, name : name, email: email});
+//     });
+// });
 
 //password hashing - login
 
-app.post("/api/users/login", async (request, response)=>{
-    const email = request.body.email;
-    const password = request.body.password;
+// app.post("/api/users/login", async (request, response)=>{
+//     const email = request.body.email;
+//     const password = request.body.password;
 
-db.query("SELECT * FROM users WHERE email=?",[ email ],async(error,result)=>{
-        console.log(error);
-    if(error) return response.status(500).json({message : "Server internal error" + error});
-    const dbPassword = result[0].password;
-    const isPasswordSame = await bcrypt.compare(password, dbPassword);
-    if(isPasswordSame){
-        response.status(200).json({message: "login successfully"})
-    }
-    else{
-    response.status(201).json({message: "login failed"})
-    }
+// db.query("SELECT * FROM users WHERE email=?",[ email ],async(error,result)=>{
+//         console.log(error);
+//     if(error) return response.status(500).json({message : "Server internal error" + error});
+//     const dbPassword = result[0].password;
+//     const isPasswordSame = await bcrypt.compare(password, dbPassword);
+//     if(isPasswordSame){
+//         response.status(200).json({message: "login successfully"})
+//     }
+//     else{
+//     response.status(201).json({message: "login failed"})
+//     }
     
-    });
+//     });
 
-}); 
+// }); 
 
 
 app.listen(3400, ()=>{
